88d3307982fed0b16a52af97cd55fc8ad204379f
vchemu <valerine.chemutai@student.moringaschool.com>
2023-12-04 20:40:25 +0300

    flask

--- server/app.py
+++ server/app.py
@@ -6,3 +6,34 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:username>')
+def print_string(username):
+    print(username)
+    return username
+
+@app.route('/count/<int:integer>')
+def count(integer):
+    count = f""
+    for i in range(integer):
+        count += f"{i}\n"
+    return count
+
+@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == "+":
+        return str(num1 + num2)
+    elif operation == "-":
+        return str(num1 - num2)
+    elif operation == "*":
+        return str(num1 * num2)
+    elif operation == "div":
+        return str(num1 / num2)
+    elif operation == "%":
+        return str(num1 % num2)
+    
+    return 'Operation invalid'
